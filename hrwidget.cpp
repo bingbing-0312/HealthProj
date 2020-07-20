266666654edd0866c5cca87e0f55641a16cdae3d
bingbing-0312 <68432896+bingbing-0312@users.noreply.github.com>
2020-07-20 12:19:18 +0800

    hrst

--- hrwidget.cpp
+++ hrwidget.cpp
@@ -1,5 +1,8 @@
 #include "hrwidget.h"
-
+#include<QLabel>
+#include<QVBoxLayout>
+#include<QHBoxLayout>
+#include<QPixmap>
 HRWidget::HRWidget(QWidget *parent) : QWidget(parent)
 {
 
@@ -12,4 +15,39 @@ void HRWidget::paintEvent(QPaintEvent *event)
     opt.init(this);
     QPainter p(this);
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+
+    hrl1->setText("80");
+    hrl1->setStyleSheet("background-color:rgb(0,255,0);");
+    hrl2->setText("bpm");
+    hrl2->setStyleSheet("background-color:rgb(0,255,0);");
+
+
+    //创建窗口对象，初始化
+    QWidget *hrleftWidget =new QWidget;
+    hrleftWidget->setStyleSheet("background-color:rgb(255,0,0);");
+
+
+    //创建布局器
+    QVBoxLayout *hrlayout =new QVBoxLayout;
+
+    //将窗口添加到布局器中
+    hrlayout->addWidget(hrleftWidget);
+
+    //80与bpm的位置
+    hrlayout->addWidget(hrleftWidget);
+    hrlayout->addWidget(hrl1,0,Qt::AlignCenter);
+    hrlayout->addWidget(hrl2,0,Qt::AlignRight|Qt::AlignTop);
+
+    hrleftWidget->setLayout(hrlayout);
+
+
+
+
+
+
+
+
+
+
+
 }
--- hrwidget.h
+++ hrwidget.h
@@ -4,14 +4,16 @@
 #include <QWidget>
 #include <QPainter>
 #include <QStyleOption>
-
+#include<QLabel>
 class HRWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit HRWidget(QWidget *parent = nullptr);
     void paintEvent(QPaintEvent *event) override;
-
+private:
+    QLabel *hrl1;
+    QLabel *hrl2;
 signals:
 
 };
