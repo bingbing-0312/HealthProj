ceba90e759aa4a286a15ef5346d07e7f8e90346b
982833040 <68454805+982833040@users.noreply.github.com>
2020-07-22 19:02:12 +0800

    病人信息界面的内容创建

--- PatientInfoWidget.h
+++ PatientInfoWidget.h
@@ -2,6 +2,11 @@
 #define PATIENTINFOWIDGET_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 class PatientInfoWidget : public QWidget
 {
@@ -9,7 +14,35 @@ class PatientInfoWidget : public QWidget
 public:
     explicit PatientInfoWidget(QWidget *parent = nullptr);
 
-signals:
+private:
+    QWidget *ptopWidget = new QWidget;
+    QWidget *pmiddle1Widget = new QWidget;
+    QWidget *pmiddle2Widget = new QWidget;
+    QWidget *pbottomWidget = new QWidget;
+    QWidget *pIDnumberWidget = new QWidget;
+    QLabel *name = new QLabel;
+    QLabel *sex = new QLabel;
+    QLabel *age = new QLabel;
+    QLabel *IDnumber = new QLabel;
+    QLineEdit *nameLineEdit = new QLineEdit;
+    QLineEdit *sexLineEdit = new QLineEdit;
+    QLineEdit *ageLineEdit = new QLineEdit;
+    QLineEdit *IDnumberLineEdit = new QLineEdit;
+    QPushButton *update = new QPushButton;
+    QPushButton *alter = new QPushButton;
+    QPushButton *save = new QPushButton;
+
+    QVBoxLayout *pwholeLayout = new QVBoxLayout;
+    QHBoxLayout *ptopLayout = new QHBoxLayout;
+    QHBoxLayout *pmiddle1Layout = new QHBoxLayout;
+    QHBoxLayout *pmiddle2Layout = new QHBoxLayout;
+    QHBoxLayout *pbottomLayout = new QHBoxLayout;
+    QHBoxLayout *pIDnumberLayout = new QHBoxLayout;
+
+private slots:
+    void savetButton_clicked();
+    void alterButton_clicked();
+
 
 };
 
