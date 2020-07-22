8b0dd9c8e2846552f52a01d2e78c3b9e2bd4e1bb
zm-xm <68309208+zm-xm@users.noreply.github.com>
2020-07-22 23:36:31 +0800

    Update DeviceSettingsWidget.h
    还有待改进

--- DeviceSettingsWidget.h
+++ DeviceSettingsWidget.h
@@ -3,6 +3,10 @@
 
 #include <QWidget>
 #include "TCPSocket.h"
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLayout>
 
 class DeviceSettingsWidget : public QWidget
 {
@@ -13,6 +17,31 @@ public:
 
 signals:
 
+private:
+    void setWindowSize(int w, int h);
+
+    QLabel *deviceID = new QLabel;
+    QLabel *badID = new QLabel;
+    QLabel *serialPortLable = new QLabel;
+    QLabel *netConnectLable = new QLabel;
+    QLabel *serialPortState = new QLabel;
+    QLabel *netConnectState = new QLabel;
+
+    QLineEdit *deviceIDEdit = new QLineEdit;
+    QLineEdit *badIDEdit = new QLineEdit;
+
+    QPushButton *deviceIDSet = new QPushButton;
+    QPushButton *badIDSet = new QPushButton;
+
+    QVBoxLayout *allLayout = new QVBoxLayout;
+    QHBoxLayout *deviceLayout = new QHBoxLayout;
+    QHBoxLayout *badLayout = new QHBoxLayout;
+    QHBoxLayout *serialPortLayout = new QHBoxLayout;
+    QHBoxLayout *netConnectLayout = new QHBoxLayout;
+    
+public slots:
+    void setDeviceID();
+    void setBadID();
 };
 
 #endif // DEVICESETTINGSWIDGET_H
